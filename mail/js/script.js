293c9029af1948f3bef0d82e666a3c8235e6f6eb
Alessio2021 <alessiobut1911@gmail.com>
2021-11-24 15:40:55 +0100

    update

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,25 +1,24 @@
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato sull’esito del controllo.
 
-const container = document.querySelector('.container')
-const div = document.createElement('div')
-const listEmail = ["alessio.butera@gmail.com", "alessiobutera@gmail.com", "ale.butera@gmail.com", "alessiobut1911@gmail.com"]
+const opacity = document.querySelector('.opacity');
+const div = document.createElement('div');
+const listEmail = ["alessio.butera@gmail.com", "alessiobutera@gmail.com", "ale.butera@gmail.com", "alessiobut1911@gmail.com"];
 
-const button = document.getElementById('btn')
+// Chiedi all’utente la sua email, 
+const button = document.getElementById('btn');
 button.addEventListener("click", function() {
     const email = document.getElementById('email').value;
-    console.log(email);
+    document.getElementById('email').value = '';
     
     for (let i = 0; i < listEmail.length; i++) {
-        
+        // controlla che sia nella lista di chi può accedere,
+        // stampa un messaggio appropriato sull’esito del controllo.
         if (email == listEmail[i]) {
-            container.append(div)
+            opacity.append(div)
             div.innerHTML = "la tua email e' nella lista"
         } else {
-            container.append(div)
+            opacity.append(div)
             div.innerHTML = "la tua email non e' nella lista"
-        }
-    }
+        };
+    };
 });
 
