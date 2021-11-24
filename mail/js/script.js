44a6f9b9e5104f3e977b365294303f33c3311a46
Alessio2021 <alessiobut1911@gmail.com>
2021-11-24 15:05:13 +0100

    email

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,2 +1,25 @@
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+const container = document.querySelector('.container')
+const div = document.createElement('div')
+const listEmail = ["alessio.butera@gmail.com", "alessiobutera@gmail.com", "ale.butera@gmail.com", "alessiobut1911@gmail.com"]
+
+const button = document.getElementById('btn')
+button.addEventListener("click", function() {
+    const email = document.getElementById('email').value;
+    console.log(email);
+    
+    for (let i = 0; i < listEmail.length; i++) {
+        
+        if (email == listEmail[i]) {
+            container.append(div)
+            div.innerHTML = "la tua email e' nella lista"
+        } else {
+            container.append(div)
+            div.innerHTML = "la tua email non e' nella lista"
+        }
+    }
+});
+
