6de50d90809700420e8e186b086e3708184f7388
Alessio2021 <alessiobut1911@gmail.com>
2021-11-24 17:20:56 +0100

    update button

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,24 +1,25 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-const container = document.querySelector(".container")
-const numbers = document.querySelector(".numbers")
-const winner = document.querySelector(".winner")
+const container = document.querySelector(".container");
+const numbers = document.querySelector(".numbers");
+const winner = document.querySelector(".winner");
+const button = document.getElementById("button");
+let randNumbUser;
+let randNumbPc;
 
-let randNumbUser = Math.floor(Math.random() * 6) + 1;
-let randNumbPc = Math.floor(Math.random() * 6) + 1;
+button.addEventListener("click", function() {
+    randNumbUser = Math.floor(Math.random() * 6 + 1);
+    randNumbPc = Math.floor(Math.random() * 6 + 1);
+    numbers.innerHTML = `Utente ` + randNumbUser + ' - ' + `Computer ` + randNumbPc;
+    
+    if (randNumbUser > randNumbPc) {
+        winner.innerHTML = "il vincitore e' l Utente"
+    } else if (randNumbPc > randNumbUser) {
+        winner.innerHTML = "il vincitore e' il Computer"
+    } else {
+        winner.innerHTML = "Parita'";
+    }
+});
 
-const div = document.createElement('div')
-
-numbers.innerHTML = `Utente ` + randNumbUser + ' - ' + `Computer ` + randNumbPc
-
-if (randNumbUser > randNumbPc) {
-    winner.append(div)
-    div.append(`Il vincitore e' l'Utente`)
-} else if (randNumbPc > randNumbUser) {
-    winner.append(div)
-    div.append(`Il vincitore e' il Computer`)
-} else {
-    winner.append(div)
-    div.append(`Parita'`)
-}
+    
